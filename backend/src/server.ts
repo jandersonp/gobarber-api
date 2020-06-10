c45d517da0399cf5f12b6c78956ed458024ba47d
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-06-10 19:16:11 -0300

    Configurando importação de arquivos TS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello GoStack' }));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('✔ Server started on post 3333!');
