bec58696ca70ce311613825b8d83b99f6e1a408e
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-06-10 17:34:52 -0300

    Configurando a estrutura do projeto

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello GoStack'})
+})
+
+app.listen(3333, () => {
+  console.log('✔ Server started on post 3333!')
+});
