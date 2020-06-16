d1d3cde4c1c13aaa473d42d2d94099e0088528f6
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-06-16 15:39:40 -0300

    chore: Gerando token JWT

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -35,7 +37,12 @@ class AuthenticateUserService {
 
     // Usuário autenticado
 
-    return { user };
+    const token = sign({}, '84452a5130ed1a85fa8ffa63ffd338db', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
