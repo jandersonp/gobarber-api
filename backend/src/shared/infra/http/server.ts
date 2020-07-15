f53d4e5c04fc3ed1fc65c9c714071b3d2b13dfd2
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-07-14 21:30:00 -0300

    chore: Configurando Imports

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -4,12 +4,12 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
-import routes from './shared/infra/http/routes';
-import uploadConfig from './config/upload';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
-import AppError from './shared/errors/AppError';
+import routes from './routes';
 
-import './shared/infra/database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
